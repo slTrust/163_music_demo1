b09990a2029a0e7d34baf1ba85ecd5f2857c87f9
huangjx <trustfor@sina.cn>
2018-05-01 00:43:44 +0800

    歌曲播放控制   播放和暂停按钮

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -2,10 +2,22 @@
     let view ={
         el:'#app',
         template:`
-            <audio autoplay controls src={{url}}></audio>
+            <audio src={{url}}></audio>
+            <div>
+                <button class="play">播放</button>
+                <button class="pause">暂停</button>
+            </div>
         `,
         render(data){
             $(this.el).html(this.template.replace('{{url}}',data.url))
+        },
+        play(){
+            let audio = $(this.el).find('audio')[0];
+            audio.play()
+        },
+        pause(){
+            let audio = $(this.el).find('audio')[0];
+            audio.pause()
         }
     }
     let model = {
@@ -31,6 +43,15 @@
             this.model.get(id).then((data)=>{
                 this.view.render(this.model.data)
             })
+            this.bindEvents()
+        },
+        bindEvents(){
+            $(this.view.el).on('click','.play',()=>{
+                this.view.play()
+            })   
+            $(this.view.el).on('click','.pause',()=>{
+                this.view.pause()
+            })            
         },
         getSongId(){
             let search = window.location.search;
