4d0b23565c3c28313368ddeafc84b01104e9cdaf
huangjx <trustfor@sina.cn>
2018-05-01 00:27:47 +0800

    播放页获取歌曲信息

---
+++ src/js/song/app.js
@@ -0,0 +1,49 @@
+{
+    let view ={}
+    let model = {
+        data:{
+            id:'',
+            name:'',
+            singer:'',
+            url:''
+        },
+        get(id){
+            var query = new AV.Query('Song');
+            return query.get(id).then((song)=>{
+                Object.assign(this.data,{id:song.id,...song.attributes})
+                return song
+            });
+        }
+    }
+    let controller = {
+        init(view,model){
+            this.view = view;
+            this.model = model;
+            let id = this.getSongId()
+            this.model.get(id).then((data)=>{
+                console.log(this.model.data)
+            })
+        },
+        getSongId(){
+            let search = window.location.search;
+            if(search.indexOf('?')===0){
+                search = search.substring(1)
+            }
+            
+            //查询字符串防止有人很贱的 放两个“&”导致解析两个空字符  home.html?id=fdsfasfd&&a=2
+            let array = search.split('&').filter((v)=>v);
+            let id = '';
+            for(let i=0;i<array.length;i++){
+                let kv = array[i].split('=')
+                let key = kv[0];
+                let value =kv[1];
+                if(key==='id'){
+                    id = value
+                }
+            }
+            return id;
+        }
+    }
+
+    controller.init(view,model)
+}
