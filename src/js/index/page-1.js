6e3c624218553ff3cd37ea260d44b2328f76a14c
huangjx <trustfor@sina.cn>
2018-04-30 23:56:14 +0800

    模块1中  子模块2获取歌曲列表

--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -1,4 +1,5 @@
 {
+
     let view ={
         el:'.page-1',
         init(){
@@ -20,6 +21,8 @@
             this.view.init();
             this.model = model;
             this.bindEventHub()
+            this.loadModule1();
+            this.loadModule2()
         },
         bindEventHub(){
             window.eventHub.on('selectTab',(tabName)=>{
@@ -29,6 +32,23 @@
                     this.view.hide()
                 }
             })
+        },
+        loadModule1(){
+            // 加载模块1里的  每个子模块
+            let script = document.createElement('script')
+            script.src = './js/index/page-1-1.js';
+            script.onload = function(){
+                console.log('模块1加载完毕')
+            }
+            document.body.appendChild(script)
+        },
+        loadModule2(){
+            let script2 = document.createElement('script')
+            script2.src = './js/index/page-1-2.js';
+            script2.onload = function(){
+                console.log('模块2加载完毕')
+            }
+            document.body.appendChild(script2)
         }
     }
     controller.init(view,model)
